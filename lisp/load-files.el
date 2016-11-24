97ef115dcf4928e60e04073daad87c51c912cbd7
Kshitij Gaipal <kgaipal@gmail.com>
2016-11-24 10:28:44 -0800

    added marmalade packages source

--- lisp/load-files.el
+++ lisp/load-files.el
@@ -1,11 +1,17 @@
 ;; Load all essential plugins/files
 ;; kgaipal@gmail.com
 
+(require 'package)
+
 ;; MELPA packages
 ;; http://stable.melpa.org/#/getting-started
-(require 'package)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
+
+;; Marmalade packages
+;; https://marmalade-repo.org/#download
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
 
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
