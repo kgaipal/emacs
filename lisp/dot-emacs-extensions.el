9dba34a56063db463274e3069c530bcd0844e42f
Kshitij Gaipal <kgaipal@gmail.com>
2017-04-30 15:11:30 -0700

    default directory is code

--- lisp/dot-emacs-extensions.el
+++ lisp/dot-emacs-extensions.el
@@ -326,7 +326,7 @@ of the frame only if it is split into exactly 2 windows."
   (require 'ripgrep)
   (if (boundp 'desktop-dirname)
       (ripgrep-regexp wildcard desktop-dirname)
-    (ripgrep-regexp wildcard "~")))
+    (ripgrep-regexp wildcard "~/code/")))
 
 (defun find-files-in-project-root-using-counsel ()
   "Find files interactively using 'counsel' package from the project ROOT."
