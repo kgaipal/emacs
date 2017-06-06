5c7b17e415c1d1001a66a257bede65f2f1e3a31b
Kshitij Gaipal <kgaipal@gmail.com>
2017-06-05 19:42:51 -0700

    moved the custom options in a file of its own

---
+++ lisp/custom-options.el
@@ -0,0 +1,44 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(Buffer-menu-name-width 35)
+ '(auto-save-default t)
+ '(custom-enabled-themes (quote (yet-another-dark)))
+ '(custom-safe-themes
+   (quote
+    ("15290cc4f813a717dfe953657368d6ba55226e9b98f10cee06b5fcd582b8340e" default)))
+ '(dired-listing-switches "-pgGhB")
+ '(electric-pair-mode t)
+ '(fill-column 90)
+ '(fringe-mode (quote (1 . 1)) nil (fringe))
+ '(global-subword-mode t)
+ '(grep-use-null-device nil)
+ '(highlight-symbol-colors (quote ("yellow")))
+ '(ido-enable-flex-matching t)
+ '(ido-mode (quote buffer) nil (ido))
+ '(indent-tabs-mode nil)
+ '(initial-buffer-choice nil)
+ '(kill-whole-line t)
+ '(make-backup-files nil)
+ '(mode-line-in-non-selected-windows t)
+ '(package-selected-packages
+   (quote
+    (gnome-c-style magit xah-find ws-butler web-mode use-package tfs spu ripgrep restart-emacs occur-x magit-gitflow ivy-rich indent-guide highlight-symbol fold-this fic-mode edit-at-point csharp-mode counsel buffer-move auto-complete anzu annotate-depth)))
+ '(read-file-name-completion-ignore-case t)
+ '(recentf-mode 1)
+ '(scroll-bar-mode nil)
+ '(scroll-conservatively 100)
+ '(sentence-end-double-space nil)
+ '(show-paren-mode t)
+ '(tab-width 8)
+ '(tool-bar-mode nil)
+ '(tramp-auto-save-directory "/tmp")
+ '(which-func-format (quote ("{" which-func-current "}"))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
