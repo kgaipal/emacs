eaec3eb73df028c0f249082839b82bb694ed52b2
Kshitij Gaipal <ksgaipal@MININT-7CKM4B1.redmond.corp.microsoft.com>
2018-06-19 09:20:00 -0700

    version bump for omni sharp 1.31.1

--- lisp/packages-customization.el
+++ lisp/packages-customization.el
@@ -6,7 +6,7 @@
     (progn
 
       ;; manually update version from https://github.com/OmniSharp/omnisharp-roslyn/releases/
-      (setq omnisharp-expected-server-version "1.30.1")))
+      (setq omnisharp-expected-server-version "1.31.1")))
 
 ;; auto upgrade packags daily
 (if (and (package-installed-p 'spu)
