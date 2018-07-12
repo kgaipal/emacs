ad3d9eb119403f44a2e0b85c15fafa423806ae2c
Kshitij Gaipal <ksgaipal@MININT-7CKM4B1.redmond.corp.microsoft.com>
2018-07-12 13:37:36 -0700

    auto start flycheck mode for C#

--- lisp/packages-customization.el
+++ lisp/packages-customization.el
@@ -5,9 +5,10 @@
 (if (package-installed-p 'omnisharp)
     (progn
 
-      ;; auto start server
+      ;; auto start server and flycheck mode
       ;; https://github.com/Omnisharp/omnisharp-emacs
       (add-hook 'csharp-mode-hook 'omnisharp-mode)
+      (add-hook 'csharp-mode-hook #'flycheck-mode)
 
       ;; manually update version from https://github.com/OmniSharp/omnisharp-roslyn/releases/
       (setq omnisharp-expected-server-version "1.32.1")))
